891491e74591b58612d572d215d5a91bd713c9d4
markiantorno <markiantorno@gmail.com>
2025-04-01 20:02:13 -0400

    initial commit

---
+++ validator-wrapper/src/main/java/com/iantorno/validator_wrapper/ValidatorWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.iantorno.validator_wrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatorWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatorWrapperApplication.class, args);
+	}
+
+}
---
+++ validator-wrapper/src/test/java/com/iantorno/validator_wrapper/ValidatorWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iantorno.validator_wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatorWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
